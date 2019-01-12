622468d91d916e7072dadd685e8c8d3b2b41ca2f
Elyse <eswoverland@elevenfiftyconsulting.com>
2019-01-11 22:18:06 -0500

    Bottom navigation

--- app/src/main/java/com/elyseswoverland/circleconnect/ui/MainActivity.kt
+++ app/src/main/java/com/elyseswoverland/circleconnect/ui/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.elyseswoverland.circleconnect.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.elyseswoverland.circleconnect.R
+import com.elyseswoverland.circleconnect.ui.account.AccountFragment
+import com.elyseswoverland.circleconnect.ui.favorites.FavoritesFragment
 import com.elyseswoverland.circleconnect.ui.map.MapFragment
+import com.elyseswoverland.circleconnect.ui.messages.MessagesFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +20,39 @@ class MainActivity : AppCompatActivity() {
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.add(R.id.content, MapFragment())
         fragmentTransaction.commit()
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.action_map -> {
+                    val fragmentManager = supportFragmentManager
+                    val fragmentTransaction = fragmentManager.beginTransaction()
+                    fragmentTransaction.replace(R.id.content, MapFragment())
+                    fragmentTransaction.commit()
+                    true
+                }
+                R.id.action_favorites -> {
+                    val fragmentManager = supportFragmentManager
+                    val fragmentTransaction = fragmentManager.beginTransaction()
+                    fragmentTransaction.replace(R.id.content, FavoritesFragment())
+                    fragmentTransaction.commit()
+                    true
+                }
+                R.id.action_messages -> {
+                    val fragmentManager = supportFragmentManager
+                    val fragmentTransaction = fragmentManager.beginTransaction()
+                    fragmentTransaction.replace(R.id.content, MessagesFragment())
+                    fragmentTransaction.commit()
+                    true
+                }
+                R.id.action_account -> {
+                    val fragmentManager = supportFragmentManager
+                    val fragmentTransaction = fragmentManager.beginTransaction()
+                    fragmentTransaction.replace(R.id.content, AccountFragment())
+                    fragmentTransaction.commit()
+                    true
+                }
+                else -> true
+            }
+        }
     }
 }
--- app/src/main/java/com/elyseswoverland/circleconnect/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/elyseswoverland/circleconnect/ui/favorites/FavoritesFragment.kt
@@ -1,4 +1,4 @@
-package com.elyseswoverland.circleconnect.ui.usernetwork
+package com.elyseswoverland.circleconnect.ui.favorites
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.elyseswoverland.circleconnect.R
 
-class NetworkFragment : Fragment() {
+class FavoritesFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_network, container, false)
