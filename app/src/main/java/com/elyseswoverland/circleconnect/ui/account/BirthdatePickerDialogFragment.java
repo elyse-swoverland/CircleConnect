c594def0a70d22e02414d5012fb8d41f578e56b9
Elyse <eswoverland@elevenfiftyconsulting.com>
2019-11-16 17:13:09 -0500

    Set logging level for releases

---
+++ app/src/main/java/com/elyseswoverland/circleconnect/ui/account/BirthdatePickerDialogFragment.java
@@ -0,0 +1,12 @@
+package com.elyseswoverland.circleconnect.ui.account;
+
+import android.widget.NumberPicker;
+
+import androidx.fragment.app.DialogFragment;
+
+public class BirthdatePickerDialogFragment extends DialogFragment {
+    public static final String TAG = BirthdatePickerDialogFragment.class.getSimpleName();
+
+    private NumberPicker yearPicker;
+    private NumberPicker monthPicker;
+}
